b03e2c648460fde9c4178058927eef3ddb304ac6
szlankapeter <115689125+szlankapeter@users.noreply.github.com>
2023-02-22 13:05:42 +0100

    Metódusok rendezése

--- Gyakorlas.java
+++ Gyakorlas.java
@@ -7,23 +7,30 @@ public class Gyakorlas {
     }
 
     private static void kiirasok() {
+        elsoKiiras();
+        masodikKiiras();
+        harmadikKiiras();
+        negyedikKiiras();
+    }
+
+    private static void elsoKiiras(){
+        kiir("Az elso 10 szam: " + elso10szamOsszege() + "\n");
+    }
+    private static void masodikKiiras(){
         int a = 1;
         int b = 5;
+        kiir(String.format("%d + %d = %d\n",a, b, osszedad(a, b)));
+    }
+    private static void harmadikKiiras(){
         int[] szamok = {2, 4, 6, 8};
         int osszeg = osszedad(szamok[0], szamok[1]);
         osszeg += osszedad(szamok[2], szamok[3]);
-        int[] szamok2 = {3, 3, 3};
-        
-        kiir("Az elso 10 szam: " + elso10szamOsszege() + "\n");
-        
-        kiir(String.format("%d + %d = %d\n",a, b, osszedad(a, b)));
-        
         kiir(String.format("%d + %d + %d + %d = %d\n", szamok[0], szamok[1], szamok[2], szamok[3], osszeg));
-        
+    }
+    private static void negyedikKiiras(){
+        int[] szamok2 = {3, 3, 3};
         kiir(String.format("%d + %d + %d gyoke = %s\n", szamok2[0], szamok2[1], szamok2[2], haromGyok(szamok2[0], szamok2[1], szamok2[2])));
     }
-    
-    
     
     
     private static int elso10szamOsszege(){
